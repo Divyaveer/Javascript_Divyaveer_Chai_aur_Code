72ea7df2a2ff2d31d3f759fcbfd04dee98087cef
Divyaveer <86354945+Divyaveer@users.noreply.github.com>
2024-04-02 18:45:21 +0530

    Datatypes Done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer
+
+//alert(3 + 3) //we are using node.js, not browser
+
+console.log( 3 
+    + 3); // code readablity should be high
+
+console.log("Divyaveer");
+
+let name = "Divyaveer"
+let age = 14
+let isLoggedIn = false
+let state;
+
+/*
+    number => 2 to the power 53
+    bigint 
+    string => ""
+    boolean => true/false
+    null => standalone value
+    undefined =>
+    symbol => unique
+*/
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
